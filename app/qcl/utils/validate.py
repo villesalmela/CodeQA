291884843df3b2646dc50cf76296fded34944a5a
Ville Salmela <villesalmela@users.noreply.github.com>
2023-09-24 23:27:42 +0300

    add function for validating email addresses

---
+++ app/qcl/utils/validate.py
@@ -0,0 +1,22 @@
+import os
+from email_validator import validate_email, EmailNotValidError
+from wtforms.validators import ValidationError
+
+
+def check_email(form, field) -> None:
+    "Custom validator that works with WTForms"
+    
+    ALLOWED_DOMAINS = [x.strip() for x in os.environ["ALLOWED_DOMAINS"].split(",")]
+    domain_ok = False
+    try:
+        emailinfo = validate_email(field.data, check_deliverability=False)
+        if emailinfo.domain in ALLOWED_DOMAINS:
+            domain_ok = True
+        message = ""
+
+    except EmailNotValidError as e:
+        message = str(e)
+    if not domain_ok:
+        message += " The application is in private preview, email domain restrictions apply."
+    if message:
+        raise ValidationError(message)
