775dcc04b02d016603404243d28453f6d27c7a3a
Ville Salmela <villesalmela@users.noreply.github.com>
2023-09-28 19:15:06 +0300

    refactor form handling for clarity

--- app/qcl/views/routes.py
+++ app/qcl/views/routes.py
@@ -17,10 +17,6 @@ from wtforms.validators import DataRequired, Length
 import logging
 logging.basicConfig(level=logging.INFO)
 
-
-logging.info(dbrunner.execute("SELECT VERSION()"))
-
-
 from flask_codemirror import CodeMirror
 codemirror = CodeMirror(app)
 
@@ -97,9 +93,9 @@ class DocForm(FlaskForm):
         Length(min=10, max=1500)
     ])
     documented = CodeMirrorField("Documented Source Code", language="python", config={"linenumbers": True}, validators=[
-        Length(max=2000)
+        Length(min=10, max=2000)
     ])
-    generate = SubmitField("Generate Docs")
+    generate = SubmitField("Generate Docs", render_kw={"formnovalidate": True})
     next = SubmitField("Next")
 
 class TestForm(FlaskForm):
@@ -107,10 +103,10 @@ class TestForm(FlaskForm):
         Length(min=10, max=2000)
     ])
     unittests = CodeMirrorField("Unit Tests", language="python", config={"linenumbers": True}, validators=[
-        Length(max=3000)
+        Length(min=10, max=3000)
     ])
-    generate = SubmitField("Generate Tests")
-    run = SubmitField("Run Tests")
+    generate = SubmitField("Generate Tests", render_kw={"formnovalidate": True})
+    run = SubmitField("Run Tests", render_kw={"formnovalidate": True})
     next = SubmitField("Next")
 
 class ClassifyForm(FlaskForm):
@@ -125,7 +121,7 @@ class ClassifyForm(FlaskForm):
         DataRequired(),
         Length(min=10, max=500)
     ])
-    generate = SubmitField("Generate keywords")
+    generate = SubmitField("Generate keywords", render_kw={"formnovalidate": True})
     save = SubmitField("Save function")
 
 @app.route("/", methods=['GET', 'POST'])
@@ -138,98 +134,135 @@ def index():
 @needs_user
 def add():
     form = CodeForm()
-    if form.validate_on_submit():
-        source_code = form.code.data
-        session["source_code"] = source_code
-        if form.lint.data:
-            filename = fileops.create_tempfile(source_code)
-            try:
-                lint_result = linter.run_pylint(filename)
-            finally:
-                fileops.delete_file(filename)
-            return render_template("add.html", form=form, lint_result=lint_result, post=True)
-        elif form.doc.data:
-            return redirect(url_for("doc"))
+    if request.method == "GET":
+        if source_code := session.get("source_code"):
+            form.code.data = source_code
+        return render_template("add.html", form=form)
         
+    elif request.method == "POST":
+        if form.validate(): #data ok
+            source_code = form.code.data
+            session["source_code"] = source_code
 
-    return render_template("add.html", form=form)
+            if form.lint.data: # clicked run linting
+                filename = fileops.create_tempfile(source_code)
+                try:
+                    lint_result = linter.run_pylint(filename)
+                finally:
+                    fileops.delete_file(filename)
+                return render_template("add.html", form=form, lint_result=lint_result)
+            
+            elif form.doc.data: # clicked next
+                return redirect(url_for("doc"))
+            
+            else: # unexpected submit
+                raise RuntimeError()
+        
+        else: # data not ok
+            return render_template("add.html", form=form)
+    else: # unexpected method
+        raise RuntimeError()
 
 @app.route("/doc", methods=["GET", "POST"])
 @needs_user
 def doc():
-    if "source_code" in session and request.method == "GET":
-        form = DocForm(code=session["source_code"])
-        del session["source_code"]
+    form = DocForm()
+    if request.method == "GET":
+        if source_code := session.get("source_code"):
+            form.code.data = source_code
+        if documented := session.get("source_code_documented"):
+            form.documented.data = documented
+        return render_template("doc.html", form=form)
+
     elif request.method == "POST":
-        form = DocForm()
-    else:
-        return redirect(url_for("badrequest"))
-    if form.validate_on_submit():
-        if form.generate.data:
+        if form.generate.data: # clicked generate tests
+            # TODO: validate source code
             documented = gpt.process_code(form.code.data, mode="doc")
             form.documented.data = documented
             return render_template("doc.html", form=form)
-        elif form.next.data:
-            session["source_code_documented"] = form.documented.data
-            return redirect(url_for("test"))
-        
-    return render_template("doc.html", form=form)
+
+        elif form.next.data: # clicked next
+            if form.validate(): # data ok
+                session["source_code"] = form.code.data
+                session["source_code_documented"] = form.documented.data
+                return redirect(url_for("test"))
+            else: # data not ok
+                return render_template("doc.html", form=form)
+        else: # unexpected submit
+            raise RuntimeError()
+    else: # unexpected method
+        raise RuntimeError()
 
 @app.route("/test", methods=["GET", "POST"])
 @needs_user
 def test():
-    if "source_code_documented" in session and request.method == "GET":
-        form = TestForm(documented=session["source_code_documented"])
-        del session["source_code_documented"]
-    elif request.method == "POST":
-        form = TestForm()
-    else:
-        return redirect(url_for("badrequest"))
-    if form.validate_on_submit():
-        if form.generate.data:
+    form = TestForm()
+    if request.method == "GET":
+        if documented := session.get("source_code_documented"):
+            form.documented.data = documented
+        if unittests := session.get("source_code_unittests"):
+            form.unittests.data = unittests
+        return render_template("test.html", form=form)
+
+    if request.method == "POST":
+        if form.generate.data: # clicked generate tests
             unittests = gpt.process_code(form.documented.data, mode="test")
             form.unittests.data = unittests
             return render_template("test.html", form=form)
-        elif form.run.data:
+        elif form.run.data: # clicked run tests
             success, results = testrunner.execute(func=form.documented.data, test=form.unittests.data)
-            if not success:
+            if not success: #TODO: handle failure
                 raise RuntimeError(results)
             results = json.dumps(results, indent=4)
             return render_template("test.html", form=form, results=results)
-        elif form.next.data:
-            session["source_code_documented"] = form.documented.data
-            session["source_code_unittests"] = form.unittests.data
-            return redirect(url_for("classify"))
-        
-    return render_template("test.html", form=form)
+        elif form.next.data: # clicked next
+            if form.validate(): # data ok
+                session["source_code_documented"] = form.documented.data
+                session["source_code_unittests"] = form.unittests.data
+                return redirect(url_for("classify"))
+            else: # data not ok
+                return render_template("test.html", form=form)
+        else: # unexpected submit
+            raise RuntimeError()
+    else: # unexpected method
+        raise RuntimeError()
+
 
 @app.route("/classify", methods=["GET", "POST"])
 @needs_user
 def classify():
-    if not ("source_code_documented" in session and "source_code_unittests" in session):
-        redirect(url_for("badrequest"))
+    expected_args = ["source_code_documented", "source_code_unittests"]
+    for arg in expected_args:
+        if not arg in session:
+            return redirect(url_for("badrequest"))
     
     form = ClassifyForm()
 
-    if request.method == "POST" and form.generate.data:
-        keywords = set(gpt.process_code(session["source_code_documented"], mode="classify"))
-        keywords_str = str(keywords).strip("}{")
-        form.keywords.data = keywords_str
+    if request.method == "GET":
         return render_template("classify.html", form=form)
-    elif form.validate_on_submit() and form.save.data:
-        keywords = {x.strip() for x in form.keywords.data.split(",")}
-        keywords_str = str(keywords).strip("}{")
-        session["function_keywords"] = keywords_str
-        session["function_name"] = form.name.data
-        session["function_usecase"] = form.usecase.data
-        return redirect(url_for("save_new_function"))
-    return render_template("classify.html", form=form)
-    
 
+    if request.method == "POST":
+        if form.generate.data: # clicked generate keywords
+            keywords = set(gpt.process_code(session["source_code_documented"], mode="classify"))
+            keywords_str = str(keywords).strip("}{")
+            form.keywords.data = keywords_str
+            return render_template("classify.html", form=form)
+        if form.save.data: # clicked save
+            if form.validate(): # data ok
+                keywords = {x.strip() for x in form.keywords.data.split(",")}
+                keywords_str = str(keywords).strip("}{")
+                session["function_keywords"] = keywords_str
+                session["function_name"] = form.name.data
+                session["function_usecase"] = form.usecase.data
+                return redirect(url_for("save_new_function"))
+            else: # data not ok
+                return render_template("classify.html", form=form)
+    
+    
 @app.route("/save_new_function")
 @needs_user
 def save_new_function():
-    expected_args = ["source_code_documented", "source_code_unittests", "function_keywords", "function_name",
+    expected_args = ["source_code", "source_code_documented", "source_code_unittests", "function_keywords", "function_name",
                      "function_usecase"]
     for arg in expected_args:
         if not arg in session:
@@ -250,7 +283,7 @@ def save_new_function():
     for arg in expected_args:
         del session[arg]
 
-    return redirect(url_for("view_function", function_id=function_id))
+    return redirect(url_for("view_function", function_id=function_id)) 
 
 @app.route("/functions/<function_id>")
 @needs_user
@@ -284,18 +317,18 @@ def login():
             return redirect(url_for("unauthorized")) 
         
         login_status = user.login(password)
-        if login_status:
+        if login_status: # all ok
             session["user_id"] = user.id
             session["user_role"] = user.role
             return redirect(url_for("index"))
-        if login_status is None:
+        elif login_status is None: # credentials ok, verification not done
             session["temp_user_id"] = user.id
             session["temp_user_role"] = user.role
             session["temp_username"] = user.name
             return render_template("email_verification.html", form=EmailVerificationForm(), error=False)
-            
-    
-        return redirect(url_for("unauthorized"))
+        else: # credentials not ok
+            return redirect(url_for("unauthorized"))
+        
     return render_template("login.html", form=form)
 
 
