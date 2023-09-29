451e8ed4b28efa24656a9eded4e81096a404562d
Ville Salmela <villesalmela@users.noreply.github.com>
2023-09-29 21:53:53 +0300

    drop unnecessary sanitization

--- app/qcl/utils/code_format.py
+++ app/qcl/utils/code_format.py
@@ -1,10 +1,10 @@
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
-from bleach import clean
 
 formatter = HtmlFormatter(linenos=True)
 
 def format(source_code: str) -> str:
-    return highlight(clean(source_code), PythonLexer(), formatter)
+    return highlight(source_code, PythonLexer(), formatter)
+
 
