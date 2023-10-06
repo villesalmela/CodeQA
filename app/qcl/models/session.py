76e1a35ae5bc61b8dcce9ae56f5471350c08ae7e
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-06 10:32:39 +0000

    extend server-side session model

--- app/qcl/models/session.py
+++ app/qcl/models/session.py
@@ -1,15 +1,14 @@
 from qcl.utils import dbrunner, compress, general
-from flask import redirect, url_for
-import flask
+from flask import redirect, url_for, g
 
 SESSION_MAX_LIFETIME = 3600
 
 class PSQLSession:
 
     @staticmethod
-    def open(session_id) -> None:
-        flask.g.psql_session_id = session_id
-        query = "SELECT created, data FROM sessions WHERE session_id=:session_id"
+    def open(session_id) -> str:
+        g.psql_session_id = session_id
+        query = "SELECT user_id, created, data FROM sessions WHERE session_id=:session_id"
         params = {"session_id": session_id}
         success, result = dbrunner.execute(query, params)
         if not success:
@@ -19,23 +18,34 @@ class PSQLSession:
             raise ValueError("Session doesn't exist")
         created = row.created
         data = row.data
-        if created > general.get_current_time + SESSION_MAX_LIFETIME:
-            return redirect(url_for("session_expired"))
-        flask.g.psql_session = compress.decompress(data)
-        flask.g.psql_session_modified = False
+        user_id = row.user_id
+        if created < general.get_current_time() - SESSION_MAX_LIFETIME:
+            raise TimeoutError("Session expired")
+        g.psql_session = compress.decompress(data)
+        g.psql_session_modified = False
+        return user_id
 
     def __setitem__(self, key, value):
-        flask.g.psql_session[key] = value
-        flask.g.psql_session_modified = True
+        g.psql_session[key] = value
+        g.psql_session_modified = True
 
     def __getitem__(self, key):
-        return flask.g.psql_session[key]
+        return g.psql_session[key]
 
     def __delitem__(self, key):
-        del flask.g.psql_session[key]
+        del g.psql_session[key]
+        g.psql_session_modified = True
 
     def __contains__(self, key):
-        return key in flask.g.psql_session
+        return key in g.psql_session
+
+    @staticmethod
+    def is_open() -> bool:
+        return hasattr(g, "psql_session")
+    
+    @staticmethod
+    def get(key):
+        return g.psql_session.get(key)
     
     @staticmethod
     def new(user_id: str, data: dict) -> str:
@@ -47,19 +57,31 @@ class PSQLSession:
             raise RuntimeError("Failed to create session")
         row = result.first()
         session_id = row.session_id
-        flask.g.psql_session = data
-        flask.g.psql_session_id = session_id
-        flask.g.psql_session_modified = False
+        g.psql_session = data
+        g.psql_session_id = session_id
+        g.psql_session_modified = False
         return session_id
 
     @staticmethod
     def save() -> None:
-        if flask.g.psql_session_modified:
+        if g.psql_session_modified:
             query = "UPDATE sessions SET data=:data WHERE session_id=:session_id"
-            data = compress.compress(flask.g.psql_session)
-            params = {"session_id": flask.g.psql_session_id, "data": data}
+            data = compress.compress(g.psql_session)
+            params = {"session_id": g.psql_session_id, "data": data}
             success, _ = dbrunner.execute(query, params)
             if not success:
                 raise RuntimeError("Failed to write session")
+            
+    @staticmethod
+    def delete() -> None:
+        query = "DELETE FROM sessions WHERE session_id=:session_id"
+        params = {"session_id": g.psql_session_id}
+        success, _ = dbrunner.execute(query, params)
+        if not success:
+            raise RuntimeError("Failed to delete session")
+        attrs = ["psql_session", "psql_session_id", "psql_session_modified"]
+        for a in attrs:
+            delattr(g, a)
+        
 
 server_session = PSQLSession()
