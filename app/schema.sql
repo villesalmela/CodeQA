b8ff9ab4667a36a0562230f70efd48c44f724424
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-06 10:23:05 +0000

    password hash function changed from pbkdf2 to scrypt: set new hash length

--- app/schema.sql
+++ app/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     username TEXT UNIQUE NOT NULL CHECK (LENGTH(username) BETWEEN 14 AND 100),
-    password TEXT NOT NULL CHECK (LENGTH(password) = 102),
-    verification_code TEXT NOT NULL CHECK (LENGTH(verification_code) = 102),
+    password TEXT NOT NULL CHECK (LENGTH(password) = 162),
+    verification_code TEXT NOT NULL CHECK (LENGTH(verification_code) = 162),
     admin BOOLEAN DEFAULT FALSE,
     verified BOOLEAN DEFAULT FALSE,
     disabled BOOLEAN DEFAULT FALSE,
