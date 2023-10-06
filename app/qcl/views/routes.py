d263b3fbd4abf9e73c9725e3cbdcf786f011a7ed
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-06 10:42:56 +0000

    update routes to use server-side session

--- app/qcl/views/routes.py
+++ app/qcl/views/routes.py
@@ -4,22 +4,26 @@ from qcl.integrations import gpt, linter, testrunner
 from qcl.models.user import User
 from qcl.models import function
 from qcl.views.forms import SignupForm, LoginForm, EmailVerificationForm, CodeForm, DocForm, TestForm, ClassifyForm
+from flask import session as client_session
+from qcl.models.session import server_session
+
 
 from functools import wraps
 import traceback
 import json
-from flask import render_template, request, session, redirect, url_for
+from flask import render_template, request, redirect, url_for, g
 
 import logging
 logging.basicConfig(level=logging.INFO)
 
 
+
 def needs_user(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if "user_role" not in session:
+        if not hasattr(g, "user"):
             return redirect(url_for("unauthorized"))
-        elif session["user_role"] not in ["user", "admin"]:
+        elif g.user.role not in ["user", "admin"]:
             return redirect(url_for("forbidden"))
         return func(*args, **kwargs)
     return wrapper
@@ -27,9 +31,9 @@ def needs_user(func):
 def needs_admin(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if "user_role" not in session:
+        if not hasattr(g, "user"):
             return redirect(url_for("unauthorized"))
-        elif session["user_role"] != "admin":
+        elif g.user.role != "admin":
             return redirect(url_for("forbidden"))
         return func(*args, **kwargs)
     return wrapper
@@ -47,7 +51,7 @@ def forbidden():
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if "user_id" not in session:
+    if "session_id" not in client_session:
         return redirect(url_for("login"))
     return render_template("index.html")
 
@@ -56,14 +60,14 @@ def index():
 def add():
     form = CodeForm()
     if request.method == "GET":
-        if source_code := session.get("source_code"):
+        if source_code := server_session.get("source_code"):
             form.code.data = source_code
         return render_template("add.html", form=form)
         
     elif request.method == "POST":
         if form.validate(): #data ok
             source_code = form.code.data
-            session["source_code"] = source_code
+            server_session["source_code"] = source_code
 
             if form.lint.data: # clicked run linting
                 filename = fileops.create_tempfile(source_code)
@@ -89,9 +93,9 @@ def add():
 def doc():
     form = DocForm()
     if request.method == "GET":
-        if source_code := session.get("source_code"):
+        if source_code := server_session.get("source_code"):
             form.code.data = source_code
-        if documented := session.get("source_code_documented"):
+        if documented := server_session.get("source_code_documented"):
             form.documented.data = documented
         return render_template("doc.html", form=form)
 
@@ -104,8 +108,8 @@ def doc():
 
         elif form.next.data: # clicked next
             if form.validate(): # data ok
-                session["source_code"] = form.code.data
-                session["source_code_documented"] = form.documented.data
+                server_session["source_code"] = form.code.data
+                server_session["source_code_documented"] = form.documented.data
                 return redirect(url_for("test"))
             else: # data not ok
                 return render_template("doc.html", form=form)
@@ -119,9 +123,9 @@ def doc():
 def test():
     form = TestForm()
     if request.method == "GET":
-        if documented := session.get("source_code_documented"):
+        if documented := server_session.get("source_code_documented"):
             form.documented.data = documented
-        if unittests := session.get("source_code_unittests"):
+        if unittests := server_session.get("source_code_unittests"):
             form.unittests.data = unittests
         return render_template("test.html", form=form)
 
@@ -138,8 +142,8 @@ def test():
             return render_template("test.html", form=form, results=results)
         elif form.next.data: # clicked next
             if form.validate(): # data ok
-                session["source_code_documented"] = form.documented.data
-                session["source_code_unittests"] = form.unittests.data
+                server_session["source_code_documented"] = form.documented.data
+                server_session["source_code_unittests"] = form.unittests.data
                 return redirect(url_for("classify"))
             else: # data not ok
                 return render_template("test.html", form=form)
@@ -154,7 +158,7 @@ def test():
 def classify():
     expected_args = ["source_code_documented", "source_code_unittests"]
     for arg in expected_args:
-        if not arg in session:
+        if not arg in server_session:
             return redirect(url_for("badrequest"))
     
     form = ClassifyForm()
@@ -164,7 +168,7 @@ def classify():
 
     if request.method == "POST":
         if form.generate.data: # clicked generate keywords
-            keywords = set(gpt.process_code(session["source_code_documented"], mode="classify"))
+            keywords = set(gpt.process_code(server_session["source_code_documented"], mode="classify"))
             keywords_str = str(keywords).strip("}{")
             form.keywords.data = keywords_str
             return render_template("classify.html", form=form)
@@ -172,9 +176,9 @@ def classify():
             if form.validate(): # data ok
                 keywords = {x.strip() for x in form.keywords.data.split(",")}
                 keywords_str = str(keywords).strip("}{")
-                session["function_keywords"] = keywords_str
-                session["function_name"] = form.name.data
-                session["function_usecase"] = form.usecase.data
+                server_session["function_keywords"] = keywords_str
+                server_session["function_name"] = form.name.data
+                server_session["function_usecase"] = form.usecase.data
                 return redirect(url_for("save_new_function"))
             else: # data not ok
                 return render_template("classify.html", form=form)
@@ -185,8 +189,8 @@ def classify():
 def delete_function(function_id: int):
     function_data = function.get_function(function_id)
     fuid = function_data["user_id"]
-    user_id = session["user_id"]
-    user_role = session["user_role"]
+    user_id = g.user.id
+    user_role = g.user.role
     if fuid == user_id or user_role == "admin":
         function.delete_function(function_id)
         return redirect(url_for("list_functions"))
@@ -199,15 +203,15 @@ def save_new_function():
     expected_args = ["source_code", "source_code_documented", "source_code_unittests", "function_keywords", "function_name",
                      "function_usecase"]
     for arg in expected_args:
-        if not arg in session:
+        if not arg in server_session:
             return redirect(url_for("badrequest"))
         
-    code = session["source_code_documented"]
-    tests = session["source_code_unittests"]
-    keywords = session["function_keywords"]
-    name = session["function_name"]
-    usecase = session["function_usecase"]
-    user_id = session["user_id"]
+    code = server_session["source_code_documented"]
+    tests = server_session["source_code_unittests"]
+    keywords = server_session["function_keywords"]
+    name = server_session["function_name"]
+    usecase = server_session["function_usecase"]
+    user_id = g.user.id
 
     save_success, function_id = function.save_function(code, tests, keywords, usecase, name, user_id)
     if not save_success:
@@ -215,7 +219,7 @@ def save_new_function():
 
     # clear session
     for arg in expected_args:
-        del session[arg]
+        del server_session[arg]
 
     return redirect(url_for("view_function", function_id=function_id)) 
 
@@ -235,10 +239,14 @@ def list_functions():
     fdata = function.list_functions()
     return render_template("functions.html", fdata=fdata)
 
+@app.route("/session_expired")
+def session_expired():
+    return render_template("session_expired.html"), 401
+
 @app.route("/login",methods=["GET", "POST"])
 def login():
 
-    if "user_id" in session:
+    if "session_id" in client_session:
         return redirect(url_for("index"))
 
     form = LoginForm()
@@ -246,59 +254,114 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         try:
-            user = User(username)
+            user = User(username=username)
         except:
             logging.error(traceback.format_exc())
             return redirect(url_for("unauthorized")) 
         
         login_status = user.login(password)
-        if login_status: # all ok
-            session["user_id"] = user.id
-            session["user_role"] = user.role
-            return redirect(url_for("index"))
-        elif login_status is None: # credentials ok, verification not done
-            session["temp_user_id"] = user.id
-            session["temp_user_role"] = user.role
-            session["temp_username"] = user.name
-            return render_template("email_verification.html", form=EmailVerificationForm(), error=False)
-        else: # credentials not ok
+        if login_status is False: # credentials not ok
             return redirect(url_for("unauthorized"))
-        
+        elif login_status is True: # all ok
+            verified = True
+        elif login_status is None: # credentials ok, verification not done
+            verified=False
+        else:
+            raise RuntimeError("Invalid login status")
+        session_id = server_session.new(user_id=user.id, data={"verified": verified})
+        client_session["session_id"] = session_id
+        return redirect(url_for("index"))
     return render_template("login.html", form=form)
 
+@app.before_request
+def pre_request():
 
-@app.route("/verify_email", methods=["POST"])
-def verify_email():
-    if "temp_user_id" not in session:
-        return redirect(url_for("unauthorized"))
+    # public access to static allowed
+    if request.path.startswith("/static/"):
+        return None
     
-    code = request.form["code"]
-    user = User(session["temp_username"])
-    valid = user.check_verification_code(code)
-    if valid:
-        session["user_id"] = session["temp_user_id"]
-        session["user_role"] = session["temp_user_role"]
-        del session["temp_user_id"]
-        del session["temp_user_role"]
-        del session["temp_username"]
-        return redirect(url_for("index"))
-    else:
-        return render_template("email_verification.html", form=EmailVerificationForm(), error=True)
+    # client thinks it has session
+    if session_id := client_session.get("session_id"):
+        try:
+            # open session
+            user_id = server_session.open(session_id) 
+            
+            # create user object and make it available globally for the duration of this request
+            g.user = User(user_id=user_id)
+        except TimeoutError:
+            del client_session["session_id"]
+            return redirect(url_for("session_expired"))
+        except:
+            # client has non-existing session_id or bad user
+            del client_session["session_id"]
+            return redirect(url_for("unauthorized"))
+
+        if server_session.get("verified"): # all ok
+            # proceed with session
+            return None
+        
+        else: # credentials ok, but email not verified
+            # allow access to verification page
+            if request.path == "/verify_email":
+                return None
+            else: # attempted normal access
+                # redirect to verification
+                return redirect(url_for("verify_email"))
+    
+    # proceed without session
+    return None
+
+@app.after_request
+def post_request(response):
+    # skip session management for static
+    if request.path.startswith("/static/"):
+        return response
+    
+    # client thinks it has session
+    if "session_id" in client_session:
+        
+        # server has session open for client
+        if server_session.is_open():
+            
+            # save session
+            server_session.save()
+    return response
+    
+
+
+@app.route("/verify_email", methods=["GET", "POST"])
+def verify_email():
+    if request.method == "GET":
+        return render_template("email_verification.html", form=EmailVerificationForm(), error=False)
+    
+    elif request.method == "POST":
+        logging.info("verifying")
+        if "session_id" not in client_session:
+            return redirect(url_for("unauthorized"))
+        
+        code = request.form["code"]
+        valid = g.user.check_verification_code(code)
+        if valid:
+            logging.info("verified")
+            server_session["verified"] = True
+            return redirect(url_for("index"))
+        else:
+            logging.info("not verified")
+            return render_template("email_verification.html", form=EmailVerificationForm(), error=True)
 
 
 @app.route("/logout")
 def logout():
-    if "user_id" in session:
-        del session["user_id"]
-    if "user_role" in session:
-        del session["user_role"]
+    if "session_id" in client_session:
+        del client_session["session_id"]
+        server_session.delete()
     return redirect(url_for("index"))
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     # already logged in
-    if "user_id" in session:
+    if "session_id" in client_session:
         logging.info("already logged in")
         return redirect(url_for("index"))
     
@@ -310,7 +373,7 @@ def register():
         password = request.form["password"]
 
         try:
-            User(username, password)
+            User.new(username, password)
             logging.info("new user success")
             return render_template("register_complete.html")
         except:
