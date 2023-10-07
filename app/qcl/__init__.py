6bd2c3835378d24ce4c7e29f6b702b6b36ffa2df
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-07 15:26:50 +0300

    setup logging

--- app/qcl/__init__.py
+++ app/qcl/__init__.py
@@ -1,9 +1,19 @@
+# setup logging
+import logging
+import sys
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(module)s: %(funcName)s: %(lineno)d]: %(message)s')
+
+stream_handler = logging.StreamHandler(sys.stderr)
+stream_handler.setFormatter(formatter)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+root_logger.addHandler(stream_handler)
+
+# do rest of imports
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-import os
-
-# config needs env variables
 from qcl.config import Config
 
 # create app and db
