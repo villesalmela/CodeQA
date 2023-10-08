cac9a06af8e61d13d1f6f3817ef333f2ae4e08b6
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-08 10:19:11 +0000

    remove unneeded configs, require needed ones

--- app/qcl/config.py
+++ app/qcl/config.py
@@ -1,13 +1,10 @@
 import os
 
-db_password = os.environ.get("DB_PASSWORD")
-db_host = os.environ.get("DB_HOST")
-db_user = os.environ.get("DB_USER")
-db_name = os.environ.get("DB_NAME")
-db_port = os.environ.get("DB_PORT")
+db_password = os.environ["DB_PASSWORD"]
+db_host = os.environ["DB_HOST"]
+db_user = os.environ["DB_USER"]
+db_name = os.environ["DB_NAME"]
+db_port = os.environ["DB_PORT"]
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require'
-    CODEMIRROR_LANGUAGES = ['python', 'yaml', 'htmlembedded']
-    CODEMIRROR_THEME = 'material'
-    WTF_CSRF_ENABLED = True
