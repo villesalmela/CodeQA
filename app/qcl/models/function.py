529ce9147478d2255fb88e06701ca7653684708f
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-22 19:03:24 +0000

    formatting

--- app/qcl/models/function.py
+++ app/qcl/models/function.py
@@ -3,23 +3,36 @@ from typing import Any
 from sqlalchemy.engine import Row
 from uuid import UUID
 
-def save_function(code: str, tests: str, keywords: str, usecase: str, name: str, user_id: UUID) -> tuple[bool, str]:
-    query = "INSERT INTO functions (code, tests, keywords, usecase, name, user_id) \
-        VALUES (:code, :tests, :keywords, :usecase, :name, :user_id) RETURNING function_id"
-    params = {"code": code, "tests": tests, "keywords": keywords, "usecase": usecase, "name": name, "user_id": user_id}
+
+def save_function(
+    code: str, tests: str, keywords: str, usecase: str, name: str, user_id: UUID
+) -> tuple[bool, str]:
+    query = """
+        INSERT INTO functions (code, tests, keywords, usecase, name, user_id)
+        VALUES (:code, :tests, :keywords, :usecase, :name, :user_id) RETURNING function_id
+        """
+    params = {
+        "code": code,
+        "tests": tests,
+        "keywords": keywords,
+        "usecase": usecase,
+        "name": name,
+        "user_id": user_id,
+    }
     try:
         result = dbrunner.execute(query, params)
     except Exception as e:
         raise RuntimeError("Failed to save function") from e
     function_id = result.first()[0]
     return function_id
-        
+
 
 def get_function(function_id: int) -> dict[str, Any]:
-    query = "SELECT f.user_id as user_id, f.name as name, f.code as code, f.tests as tests, f.usecase as usecase, f.keywords as keywords, u.username as username \
-        FROM functions AS f \
-        JOIN users AS u ON f.user_id = u.user_id \
-        WHERE f.function_id = :function_id;"
+    query = """SELECT f.user_id as user_id, f.name as name, f.code as code, f.tests as tests, f.usecase as usecase, f.keywords as keywords, u.username as username
+        FROM functions AS f
+        JOIN users AS u ON f.user_id = u.user_id
+        WHERE f.function_id = :function_id
+        """
     params = {"function_id": function_id}
     try:
         result = dbrunner.execute(query, params)
@@ -30,12 +43,16 @@ def get_function(function_id: int) -> dict[str, Any]:
         raise ValueError("Function not found")
     return row._asdict()
 
+
 def list_functions() -> list[Row]:
-    query = """SELECT f.function_id, f.name as name, f.usecase as usecase, f.keywords as keywords, u.username as username, r.average as average
+    query = """
+        SELECT f.function_id, f.name as name, f.usecase as usecase, f.keywords as keywords, u.username as username, r.average as average
         FROM functions AS f
         JOIN users AS u ON f.user_id = u.user_id
         LEFT JOIN (
-            SELECT function_id, ROUND(AVG(value), 1) as average FROM ratings GROUP BY function_id
+            SELECT function_id, ROUND(AVG(value), 1) as average
+            FROM ratings
+            GROUP BY function_id
         ) AS r ON f.function_id = r.function_id
         """
     try:
@@ -43,6 +60,7 @@ def list_functions() -> list[Row]:
     except Exception as e:
         raise RuntimeError("Failed to list functions") from e
     return result.all()
+
 
 def list_functions_by_user(user_id: UUID) -> list[Row]:
     query = """
@@ -61,8 +79,12 @@ def list_functions_by_user(user_id: UUID) -> list[Row]:
         raise RuntimeError("Failed to list functions") from e
     return result.all()
 
+
 def delete_function(function_id: int) -> None:
-    query = "DELETE FROM functions WHERE function_id = :function_id;"
+    query = """
+        DELETE FROM functions
+        WHERE function_id = :function_id
+        """
     params = {"function_id": function_id}
     try:
         dbrunner.execute(query, params)
--- app/qcl/models/search.py
+++ app/qcl/models/search.py
@@ -1,8 +1,16 @@
-from qcl.utils import dbrunner
 import json
+from qcl.utils import dbrunner
+
 
 class SearchResult:
-    def __init__(self, result_type: str, function_id: int, function_name: str, context: str, match: str) -> None:
+    def __init__(
+        self,
+        result_type: str,
+        function_id: int,
+        function_name: str,
+        context: str,
+        match: str,
+    ) -> None:
         self.result_type = result_type
         self.function_id = function_id
         self.function_name = function_name
@@ -11,26 +19,42 @@ class SearchResult:
 
     def __str__(self) -> str:
         return json.dumps(
-            {"result_type": self.result_type,
-             "function_id": self.function_id,
-             "function_name": self.function_name,
-             "context": self.context,
-             "match": self.match}
+            {
+                "result_type": self.result_type,
+                "function_id": self.function_id,
+                "function_name": self.function_name,
+                "context": self.context,
+                "match": self.match,
+            }
         )
 
+
 def search_keywords(arg: str) -> list[SearchResult]:
-    
-    query = "SELECT function_id, name, keywords FROM functions WHERE keywords ILIKE :arg"
+    query = """
+        SELECT function_id, name, keywords
+        FROM functions
+        WHERE keywords ILIKE :arg
+        """
     target_field = "keywords"
     return _search(arg, query, target_field)
 
+
 def search_usecase(arg: str) -> list[SearchResult]:
-    query = "SELECT function_id, name, usecase FROM functions WHERE usecase ILIKE :arg"
+    query = """
+        SELECT function_id, name, usecase
+        FROM functions
+        WHERE usecase ILIKE :arg
+        """
     target_field = "usecase"
     return _search(arg, query, target_field)
 
+
 def search_code(arg: str) -> list[SearchResult]:
-    query = "SELECT function_id, name, code FROM functions WHERE code ILIKE :arg"
+    query = """
+        SELECT function_id, name, code
+        FROM functions
+        WHERE code ILIKE :arg
+        """
     target_field = "code"
     funcs = _search(arg, query, target_field)
     out = []
@@ -38,10 +62,18 @@ def search_code(arg: str) -> list[SearchResult]:
         lines = func.context.splitlines()
         for line in lines:
             if arg in line:
-                out.append(SearchResult(result_type="source code", function_id=func.function_id,
-                                        function_name=func.function_name, context=line, match=arg))
-                
+                out.append(
+                    SearchResult(
+                        result_type="source code",
+                        function_id=func.function_id,
+                        function_name=func.function_name,
+                        context=line,
+                        match=arg,
+                    )
+                )
+
     return out
+
 
 def _search(arg: str, query: str, target_field: str) -> list[SearchResult]:
     out = []
@@ -51,7 +83,7 @@ def _search(arg: str, query: str, target_field: str) -> list[SearchResult]:
 
     # sanitize the argument by escaping user-added wildcards
     arg = arg.replace("%", "\\%").replace("_", "\\_")
-    
+
     # add wildcards
     arg = f"%{arg}%"
 
@@ -59,6 +91,13 @@ def _search(arg: str, query: str, target_field: str) -> list[SearchResult]:
     result = dbrunner.execute(query, params)
     rows = result.all()
     for row in rows:
-        out.append(SearchResult(result_type=target_field, function_id=row.function_id,
-                                function_name=row.name, context=getattr(row, target_field), match=arg_orig))
+        out.append(
+            SearchResult(
+                result_type=target_field,
+                function_id=row.function_id,
+                function_name=row.name,
+                context=getattr(row, target_field),
+                match=arg_orig,
+            )
+        )
     return out
